d31a15df017b7bf2f7bf8cd57f58154ace8f41d8
danielpaquette <dpaquette@petrichor.io>
2015-12-16 16:25:18 -0500

    Initial file structure, build process, dependencies.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lightweight Modular CSS Boilerplate
+
+[![Build Status](https://travis-ci.org/danpaquette/lwm-css-boilerplate.svg?branch=master)](https://travis-ci.org/danpaquette/danpaquette.github.io)
+[![Dependencies](https://david-dm.org/danpaquette/lwm-css-boilerplate.svg)](https://david-dm.org/danpaquette/danpaquette.github.io)
+[![Development Dependencies](https://david-dm.org/danpaquette/lwm-css-boilerplate/dev-status.svg)](https://david-dm.org/danpaquette/danpaquette.github.io#info=devDependencies)
+
 The Lightweight Modular CSS Boilerplate provides a set of baseline styles and tools to compose websites and web applications.
---
+++ gruntfile.js
@@ -0,0 +1,102 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks("grunt-sass");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-scss-lint");
+	grunt.loadNpmTasks("grunt-postcss");
+
+	grunt.initConfig({
+
+		/**
+		 * Lints scss.
+		 */
+		scsslint: {
+			allFiles: [
+				"src/**/*.scss"
+			],
+			options: {
+				config: "config/build/scsslint.yml",
+				colorizeOutput: true,
+				maxBuffer: 3000 * 1024
+			}
+		},
+
+		/**
+		 * Deletes all files and directories as specified.
+		 */
+		clean: {
+			dist: [
+				"dist"
+			]
+		},
+
+		/**
+		 * Compiles scss files.
+		 */
+		sass: {
+			options: {
+				sourceMap: false,
+				outputStyle: "compressed"
+			},
+			src: {
+				files: {
+					"dist/main.min.css" : "src/main.scss",
+					"dist/grid.min.css" : "src/grid.scss",
+					"dist/buttons.min.css" : "src/buttons.scss",
+					"dist/code.min.css" : "src/code.scss",
+					"dist/lists.min.css" : "src/lists.scss",
+					"dist/tables.min.css" : "src/tables.scss",
+					"dist/typography.min.css" : "src/typography.scss",
+					"dist/forms.min.css" : "src/forms.scss"
+				}
+			}
+		},
+
+		/**
+		 * Post processor for automatically adding browser prefixes.
+		 */
+		postcss: {
+			options: {
+				map: false,
+				processors: [
+					require("autoprefixer")({
+						browsers: ["last 2 versions"]
+					})
+				]
+			},
+			dist: {
+				src: "dist/**/*.css"
+			}
+		},
+
+		/**
+		 * Concatenates and minifies CSS files.
+		 */
+		cssmin: {
+			options: {
+				sourceMap: false
+			},
+			main: {
+				files: {
+					"dist/main.min.css": [
+						"bower_components/normalize-css/normalize.css",
+						"dist/main.min.css"
+					]
+				}
+			}
+		}
+
+	});
+
+	/**
+	 * DEFAULT
+	 */
+	grunt.registerTask("default", [
+		"scsslint",
+		"clean:dist",
+		"sass:src",
+		"postcss:dist",
+		"cssmin:main"
+	]);
+};
